8383709080979700ce1d6dfdf267cb6941cbda55
Ricardo Delgado <ridel007@gmail.com>
2017-10-25 12:11:47 -0500

    Update and rename CompartirContacto.java to ShareContacts.java

---
+++ src/main/java/com/project/freeboard/mensajes/ShareContacts.java
@@ -0,0 +1,14 @@
+package com.project.freeboard.mensajes;
+
+public class ShareContacts {
+
+
+	public ShareContacts() {
+	}
+
+	public boolean sendMessage(String emailStudent, String emailBusiness) {
+		return false;
+		
+	}
+
+}
