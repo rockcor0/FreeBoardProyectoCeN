6838e2d260b8fd135ba5a4bd87ee705fd16f15b5
rocco522 <ridel007@gmail.com>
2017-10-27 12:15:48 -0500

    Actualización

---
+++ target/generated-sources/annotations/com/project/freeboard/entity/Transactions_.java
@@ -0,0 +1,29 @@
+package com.project.freeboard.entity;
+
+import javax.persistence.metamodel.*;
+
+@StaticMetamodel(Transactions.class)
+public class Transactions_
+{
+    public static volatile SingularAttribute<Transactions, Long> serialVersionUID;
+    public static volatile SingularAttribute<Transactions, java.lang.String> referenceCode;
+    public static volatile SingularAttribute<Transactions, java.lang.String> description;
+    public static volatile SingularAttribute<Transactions, Double> amount;
+    public static volatile SingularAttribute<Transactions, Double> tax;
+    public static volatile SingularAttribute<Transactions, Integer> taxReturnBase;
+    public static volatile SingularAttribute<Transactions, java.lang.String> currency;
+    public static volatile SingularAttribute<Transactions, java.lang.String> buyerFullName;
+    public static volatile SingularAttribute<Transactions, java.lang.String> buyerEmail;
+    public static volatile SingularAttribute<Transactions, java.lang.String> test;
+    public static volatile SingularAttribute<Transactions, java.lang.String> payHash;
+    public static volatile SingularAttribute<Transactions, java.lang.Integer> statePol;
+    public static volatile SingularAttribute<Transactions, java.lang.Integer> responseCodePol;
+    public static volatile SingularAttribute<Transactions, java.lang.String> responseMessagePol;
+    public static volatile SingularAttribute<Transactions, java.lang.Integer> paymentMethodType;
+    public static volatile SingularAttribute<Transactions, java.util.Date> transactionDate;
+    public static volatile SingularAttribute<Transactions, java.lang.String> paymentMethodName;
+    public static volatile SingularAttribute<Transactions, java.lang.String> transactionscol;
+    public static volatile SingularAttribute<Transactions, java.util.Date> created;
+    public static volatile SingularAttribute<Transactions, java.util.Date> updated;
+    public static volatile SingularAttribute<Transactions, com.project.freeboard.entity.Offers> offersIdoffers;
+}
